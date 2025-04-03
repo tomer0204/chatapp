f24535cebbf33cc18cded84b1d0d43d4d8b0a30b
Tomer doron <tomer020406@gmail.com>
2025-04-04 01:24:49 +0300

    added the whole frontend part need to continue on the backend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
+
+const styles = {
+	global: (props) => ({
+		body: {
+			color: mode('gray.800', 'whiteAlpha.900')(props),
+			bg: mode('gray.100', '#101010')(props),
+		}
+	})
+};
+
+const config = {
+	initialColorMode: 'dark',
+	useSystemColorMode: true,
+};
+
+const colors = {
+	gray: {
+		light: '#616161',
+		dark: '1e1e1e'
+	}
+};
+
+const theme = extendTheme({ config, styles, colors });
+
+createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<RecoilRoot>
+			<BrowserRouter>
+				<ChakraProvider theme={theme}>
+					<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+					<SocketContextProvider>
+						<App />
+					</SocketContextProvider>
+				</ChakraProvider>
+			</BrowserRouter>
+		</RecoilRoot>
+	</StrictMode>,
+);
