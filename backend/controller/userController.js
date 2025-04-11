4d439f7faecf71587a3fa400b31267de5bad624b
tomer doron <tomer020406@gmail.com>
2025-04-11 17:24:55 +0300

    changed a type error in the userModel file and updated the userController

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,7 +3,28 @@ import User from "../models/userModel.js";
 const signUpUser = async (req, res) => {
 	try{
 		const {name,email,username,password} = req.body;
-		const user = await User.findOne({$or: [{email}, {username}]});
+		const user = await User.findOne({$or:[{email},{username}]});
+
+		if(user){
+			return res.status(400).json({message:"User already exists"});
+		}
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(password, salt);
+
+		const newUser = new User({name, email, username, password:hashPassword});
+		await newUser.save();
+		if(newUser){
+			res.status(201).json({
+				id:newUser.id,
+				name:newUser.name,
+				email:newUser.email,
+				username:newUser.username,
+				password:newUser.password
+			});
+		}
+		else {
+			res.status(400).json({message:"Invalid data try again"});
+		}
 	}
 	catch(err){
 		res.status(500).json({message: err.message});
