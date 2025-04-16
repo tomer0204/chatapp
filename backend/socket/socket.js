7431543009e395d9ffe1bf16368f476d9bb5f9c1
Tomer doron <tomer020406@gmail.com>
2025-04-16 20:23:09 +0300

    did != and not !==

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
+		origin: ["http://localhost:3000", "https://chatapp-8328.onrender.com"],
 		methods: ["GET", "POST"],
 	},
 });
@@ -23,7 +23,7 @@ io.on("connection", (socket) => {
 	console.log("user connected", socket.id);
 	const userId = socket.handshake.query.userId;
 
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
+	if (userId !== "undefined") userSocketMap[userId] = socket.id;
 	io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
 	socket.on("markMessagesAsSeen", async ({ conversationId, userId }) => {
